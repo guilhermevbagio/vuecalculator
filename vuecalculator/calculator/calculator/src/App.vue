1ea3be1a596d653d129e54b54d3a75c1bd57e2ae
Guilherme <guilhermevbagio@gmail.com>
2024-08-12 11:48:52 -0300

    drag and drop nao rola

--- vuecalculator/calculator/calculator/src/App.vue
+++ vuecalculator/calculator/calculator/src/App.vue
@@ -1,16 +1,47 @@
-<script setup>
-import HomePage from './components/HomePage.vue';
-</script>
-
 <template>
-  <header>
-
-
-  </header>
-
   <main>
-    <div class="flex justify-center items-center h-screen bg-gray-500">
-      <HomePage/>
+    <div class="flex justify-center items-center h-screen bg-gray-500 cursor-pointer" >
+
+      <div>
+
+        <div
+          class="p-4 transition-transform ease-out duration-300"
+          :style="{ transform: `translateX(${position}px)` }"
+          @mousedown="startDrag" @drag ="onDrag" @dragend="stopDrag"
+          >
+          <HomePage :class="'mr-[distance]'" />
+        </div>
+        
+
+      </div>
     </div>
+
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import HomePage from './components/HomePage.vue';
+
+// Define reactive state
+const position = ref(0);
+const isDragging = ref(false);
+const startX = ref(0);
+
+const startDrag = (event) => {
+  isDragging.value = true;
+  startX.value = event.clientX - position.value;
+
+};
+
+
+const onDrag = (event) => {
+  if(isDragging.value) position.value = (event.clientX - startX.value);
+};
+
+const stopDrag = () => {
+  isDragging.value = false;
+};
+
+</script>
+
