c8091a01b4e947db89e6254525399d286aeabc32
Guilherme <guilhermevbagio@gmail.com>
2024-08-13 09:02:49 -0300

    Update App.vue

--- vuecalculator/calculator/calculator/src/App.vue
+++ vuecalculator/calculator/calculator/src/App.vue
@@ -2,46 +2,17 @@
   <main>
     <div class="flex justify-center items-center h-screen bg-gray-500 cursor-pointer" >
 
-      <div>
 
-        <div
-          class="p-4 transition-transform ease-out duration-300"
-          :style="{ transform: `translateX(${position}px)` }"
-          @mousedown="startDrag" @drag ="onDrag" @dragend="stopDrag"
-          >
-          <HomePage :class="'mr-[distance]'" />
-        </div>
+          <HomePage/>
         
 
-      </div>
     </div>
 
   </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import HomePage from './components/HomePage.vue';
-
-// Define reactive state
-const position = ref(0);
-const isDragging = ref(false);
-const startX = ref(0);
-
-const startDrag = (event) => {
-  isDragging.value = true;
-  startX.value = event.clientX - position.value;
-
-};
-
-
-const onDrag = (event) => {
-  if(isDragging.value) position.value = (event.clientX - startX.value);
-};
-
-const stopDrag = () => {
-  isDragging.value = false;
-};
 
 </script>
 
